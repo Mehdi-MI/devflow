4a75e92a646d238363d43542edf31b61c0b6d13c
Hasbellaoui Mehdi <hasbellaouimehdi@gmail.com>
2026-07-31 21:41:10 +0100

    Initialize Spring Boot backend and PostgreSQL environment

---
+++ backend/src/test/java/com/mehdi/devflow/DevflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehdi.devflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
